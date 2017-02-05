da28879fd94055c0ff4b6150c8742ca41344d8de
Peter Deal <pdeal@nl.rogers.com>
2017-02-05 12:40:31 -0330

    add special weapons

--- exact.py
+++ exact.py
@@ -26,23 +26,28 @@ def B(k, n, p):
     return nCr(n, k) * p**k * (1-p)**(n-k)
 
 @lru_cache(maxsize = None)
-def playerA_win_prob(ahp, bhp, turn):
+def playerA_win_prob(ahp, bhp, turn, w_attacks_left):
     if ahp <= 0: return 0
     if bhp <= 0: return 1
-    n_a = a['attack']
+    if w_attacks_left > 0:
+        n_a = a['w_attack']
+    else:
+        n_a = a['attack']
     n_b = b['attack']
     p_a = 1 - Fraction(b['defense'], 6) 
     p_b = 1 - Fraction(a['defense'], 6) 
     chance_if_A_hit = Fraction(0, 1)
     for i in range(a['attack']):
-        chance_if_A_hit += B(i + 1, n_a, p_a) * playerA_win_prob(ahp, bhp - i - 1, TURN_B)
+        new_attacks_left = w_attacks_left - 1
+        if new_attacks_left < 0: new_attacks_left = 0
+        chance_if_A_hit += B(i + 1, n_a, p_a) * playerA_win_prob(ahp, bhp - i - 1, TURN_B, new_attacks_left)
     chance_if_B_hit = Fraction(0, 1)
     for i in range(b['attack']):
-        chance_if_B_hit += B(i + 1, n_b, p_b) * playerA_win_prob(ahp - i - 1, bhp, TURN_A)
+        chance_if_B_hit += B(i + 1, n_b, p_b) * playerA_win_prob(ahp - i - 1, bhp, TURN_A, w_attacks_left)
     if turn == TURN_A:
         return (chance_if_A_hit + B(0, n_a, p_a) * chance_if_B_hit) / (1 - B(0, n_a, p_a) * B(0, n_b, p_b))
     else: 
         return (chance_if_B_hit + B(0, n_b, p_b) * chance_if_A_hit) / (1 - B(0, n_a, p_a) * B(0, n_b, p_b))
 
-chance = float(playerA_win_prob(a['health'], b['health'], TURN_A))
+chance = float(playerA_win_prob(a['health'], b['health'], TURN_A, a['w_times']))
 print("Chance of", a["Which"], "winning is", chance*100, "%")
